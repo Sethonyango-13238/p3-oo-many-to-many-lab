53ea80b7fae7bf33d4b76449b530c46c8f1362a2
Sethonyango-13238 <onyangosethoyugi@gmail.com>
2025-08-26 10:44:30 +0300

    Initial commit

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,70 @@
+from datetime import datetime
+
 class Author:
-    pass
+    all = []
+
+    def __init__(self, name):
+        self.name = name
+        Author.all.append(self)
+
+    def contracts(self):
+        """Return all contracts associated with this author"""
+        return [c for c in Contract.all if c.author == self]
+
+    def books(self):
+        """Return all books this author has contracts with"""
+        return [c.book for c in self.contracts()]
+
+    def sign_contract(self, book, date, royalties):
+        """Create a new contract for this author and book"""
+        return Contract(self, book, date, royalties)
+
+    def total_royalties(self):
+        """Sum of all royalties from this author's contracts"""
+        return sum(c.royalties for c in self.contracts())
 
 
 class Book:
-    pass
+    all = []
+
+    def __init__(self, title):
+        self.title = title
+        Book.all.append(self)
+
+    def contracts(self):
+        """Return all contracts associated with this book"""
+        return [c for c in Contract.all if c.book == self]
+
+    def authors(self):
+        """Return all authors who have contracts with this book"""
+        return [c.author for c in self.contracts()]
 
 
 class Contract:
-    pass
+    all = []
+
+    def __init__(self, author, book, date, royalties):
+        # validations
+        if not isinstance(author, Author):
+            raise Exception("author must be an Author instance")
+        if not isinstance(book, Book):
+            raise Exception("book must be a Book instance")
+        if not isinstance(date, str):
+            raise Exception("date must be a string")
+        if not isinstance(royalties, int):
+            raise Exception("royalties must be an int")
+
+        self.author = author
+        self.book = book
+        self.date = date
+        self.royalties = royalties
+        Contract.all.append(self)
+
+    @classmethod
+    def contracts_by_date(cls, date):
+        """Return contracts matching the given date, sorted by date"""
+        # Filter contracts by the given date
+        matching = [c for c in cls.all if c.date == date]
+        # Sort by date (even if same, preserve order)
+        return sorted(matching, key=lambda c: datetime.strptime(c.date, "%d/%m/%Y"))
+
